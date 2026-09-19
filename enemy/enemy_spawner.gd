4563c0cb324b9f723699c4c57d8fb5b5b153723a
HaroldHuynh <hhuynh1228@gmail.com>
2026-09-19 14:24:53 -0700

    set max enemies

--- enemy/enemy_spawner.gd
+++ enemy/enemy_spawner.gd
@@ -6,7 +6,7 @@ signal hit_p
 
 var enemy_scene = preload("res://enemy/enemy.tscn")
 var spawn_points := []
-
+var max_enemies = 5
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -16,13 +16,14 @@ func _ready() -> void:
 
 
 func _on_timer_timeout() -> void:
-	
-	var spawn = spawn_points[randi() % spawn_points.size()]
-	var enemy = enemy_scene.instantiate()
-	enemy.position = spawn.position
-	enemy.hit_player.connect(hit)
-	level.add_child(enemy)
-	enemy.add_to_group("enemies")
+	var enemies = get_tree().get_nodes_in_group("enemies")
+	if enemies.size < max_enemies:	
+		var spawn = spawn_points[randi() % spawn_points.size()]
+		var enemy = enemy_scene.instantiate()
+		enemy.position = spawn.position
+		enemy.hit_player.connect(hit)
+		level.add_child(enemy)
+		enemy.add_to_group("enemies")
 
 func hit():
 	hit_p.emit()
