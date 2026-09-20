b5bec4ef09afaf35514a7ea0e7d63d09558b2580
nicolashlam <nicolas.h.lam@gmail.com>
2026-09-19 21:14:30 -0700

    working on bomb firing

---
+++ levels/bomb.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+var direction := Vector2.DOWN
+@export var speed := 2000.0
+
+func aim_at(target_position: Vector2) -> void:
+	direction = global_position.direction_to(target_position)
+
+	# Your laser art points downward by default.
+	rotation = direction.angle() - PI / 2
+
+func _physics_process(delta: float) -> void:
+	$AnimatedSprite2D.play("default")
+	global_position += direction * speed * delta
+	
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.name == "player":
+		get_parent()._on_enemy_spawner_hit_p()
+		queue_free()
+		
+	
--- levels/boss.gd
+++ levels/boss.gd
@@ -1,7 +1,9 @@
 extends CharacterBody2D
 
 @export var laser_scene: PackedScene
-@export var fire_every_seconds := 5.0
+@export var bomb_scene: PackedScene
+@export var laser_every_seconds := 5.0
+@export var bomb_every_seconds := 7.0
 @export var warning_seconds := 2.0
 
 var is_attacking := false
@@ -15,12 +17,18 @@ func _ready() -> void:
 	hp = 12
 	
 	$AnimatedSprite2D.play("default")
-
-	var fire_timer := Timer.new()
-	fire_timer.wait_time = fire_every_seconds
-	fire_timer.autostart = true
-	fire_timer.timeout.connect(fire_laser)
-	add_child(fire_timer)
+#timer for laser
+	var laser_timer := Timer.new()
+	laser_timer.wait_time = laser_every_seconds
+	laser_timer.autostart = true
+	laser_timer.timeout.connect(fire_laser)
+	add_child(laser_timer)
+#timer for bombs
+	var bomb_timer := Timer.new()
+	bomb_timer.wait_time = bomb_every_seconds
+	bomb_timer.autostart = true
+	bomb_timer.timeout.connect(fire_bomb)
+	add_child(bomb_timer)
 
 func fire_laser() -> void:
 	if is_attacking:
@@ -49,3 +57,27 @@ func fire_laser() -> void:
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	hit_player.emit()
+
+func fire_bomb() -> void:
+	if is_attacking:
+		return
+
+	is_attacking = true
+
+	# Warning: play the attack animation before the bomb exists.
+	$AnimatedSprite2D.play("attack")
+	await get_tree().create_timer(warning_seconds).timeout
+
+	# Fire the bomb after the two-second warning.
+	var bomb = bomb_scene.instantiate()
+	get_parent().add_child(bomb)
+	bomb.global_position = global_position
+
+	var player = get_tree().get_first_node_in_group("player")
+	if player:
+		bomb.aim_at(player.global_position)
+	else:
+		push_warning("Boss could not find a node in the player group.")
+
+	$AnimatedSprite2D.play("default")
+	is_attacking = false
