e650d1b6680f7a6ada267536457b79daf0946c68
MatthewLi-Y <zicai573@gmail.com>
2026-09-19 21:12:06 -0700

    added pause menu and its script; instantiated it to level 1

--- main menu/texture_rect.gd
+++ main menu/texture_rect.gd
@@ -1,8 +1,8 @@
-extends TextureRect
+extends CanvasLayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$credits.hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -10,18 +10,17 @@ func _process(delta: float) -> void:
 	pass
 	
 const GAME_SCENE = "res://levels//level_1.tscn"
-const CREDITS_SCENE = "res://main menu//credits.tscn"
-
-# Connect from your invisible StartButton node's pressed() signal
-func _on_start_button_pressed() -> void:
-	print("Start button was clicked!")
-	get_tree().change_scene_to_file(GAME_SCENE)
-
-# Connect from your invisible SettingsButton node's pressed() signal
-func _on_credits_button_pressed() -> void:
-	print("Credits button was pressed!")
-	get_tree().change_scene_to_file(CREDITS_SCENE)
-
+const CREDITS_SCENE = "res://main menu//credits.tscn"	
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_play_pressed() -> void:
+	print("Start button was clicked!")
+	get_tree().change_scene_to_file(GAME_SCENE)
+
+
+func _on_credits_pressed() -> void:
+	print("Credits button was pressed!")
+	$credits.show()
---
+++ pause menu/pause_menu.gd
@@ -0,0 +1,37 @@
+extends CanvasLayer
+
+func _ready() -> void:
+	# Hide the pause menu immediately when the game starts
+	hide()
+
+func _input(event: InputEvent) -> void:
+	# "pause" maps to the Escape key by default in Godot
+	if event.is_action_pressed("pause"):
+		toggle_pause()
+
+func toggle_pause() -> void:
+	# Invert the current pause state of the engine tree
+	get_tree().paused = !get_tree().paused
+	
+	# Show the menu if paused, hide it if unpaused
+	if get_tree().paused:
+		show()
+		# If you are making a 3D game or mouse-captured game:
+		# Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	else:
+		hide()
+		# Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _on_resume_button_pressed() -> void:
+	# Unpause the game and hide this menu
+	toggle_pause()
+
+func _on_quit_button_pressed() -> void:
+	# Exit the game cleanly
+	get_tree().quit()
+
+func _on_main_menu_pressed() -> void:
+	get_tree().change_scene_to_file("res://main menu/main menu.tscn")
+
+func _on_main_menu_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://main menu/main menu.tscn")
