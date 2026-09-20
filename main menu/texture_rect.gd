63c61e8da716d8b10a31127169753a20ffc0c5f5
HaroldHuynh <hhuynh1228@gmail.com>
2026-09-20 16:07:40 -0700

    fix main menu functionality

--- main menu/texture_rect.gd
+++ main menu/texture_rect.gd
@@ -3,6 +3,7 @@ extends CanvasLayer
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$credits.hide()
+	get_tree().paused = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
