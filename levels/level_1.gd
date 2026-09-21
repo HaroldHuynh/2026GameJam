01edc6a0674b57c331555d86f69ed077edffd937
HaroldHuynh <hhuynh1228@gmail.com>
2026-09-20 18:24:36 -0700

    add Win Screen

--- levels/level_1.gd
+++ levels/level_1.gd
@@ -21,6 +21,7 @@ func _process(delta: float) -> void:
 	pass
 
 func new_game():
+	$WinScreen.hide()
 	$GameOver.hide()
 	$LevelUpWindow.hide()
 	$Hud.show()
@@ -102,3 +103,7 @@ func speedUp():
 	$Hud/LevelLabel.text = "Windows 1." + str(level)
 	exp = 0
 	$Hud/EXPLabel.text = "Update Progress: " + str(exp) + "%"
+	
+func win():
+	get_tree().paused = true
+	$WinScreen.show()
